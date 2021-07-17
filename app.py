ebcf92ea33b0181cc0c81ff110b18d9699c2a366
antoniotorone <antoniotorone8913@gmail.com>
2021-07-17 19:40:16 +0000

    Registration post method added

--- app.py
+++ app.py
@@ -27,6 +27,23 @@ def get_recipes():
 
 @app.route("/register", methods=["GET", "POST"])
 def register():
+    if request.method == "POST":
+        existing_user = mongo.db.users.find_one(
+            {"username": request.form.get("username").lower()})
+
+        if existing_user:
+            flash("Username already used")
+            return redirect(url_for("register"))
+
+        register = {
+            "username": request.form.get("username").lower(),
+            "password": generate_password_hash(request.form.get("password"))
+        }
+        mongo.db.users.insert_one(register)
+
+        #cookie
+        session["user"] = request.form.get("username").lower()
+        flash("Registration Completed Successfully")
     return render_template("register.html")
 
 
