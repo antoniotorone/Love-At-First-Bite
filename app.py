88d0df2cff349b02c871a305b3a0f284105a8fd8
antoniotorone <antoniotorone8913@gmail.com>
2021-07-30 20:35:01 +0000

    home and login page layout modified

--- app.py
+++ app.py
@@ -69,7 +69,6 @@ def login():
             if check_password_hash(
                 existing_user["password"], request.form.get("password")):
                     session["user"] = request.form.get("username").lower()
-                    flash("Welcome, {}".format(request.form.get("username")))
                     return redirect(url_for(
                         "profile", username=session["user"]))
             else:
@@ -121,7 +120,7 @@ def create_recipe():
         }
         mongo.db.recipes.insert_one(recipes)
         flash("Recipe created successfully")
-        return redirect(url_for("get_recipes"))
+        return redirect(url_for("get_recipes",))
 
 
     categories = mongo.db.category.find().sort("category_name", 1)
